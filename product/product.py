1ecf4cd1de6068528f13f54d7edac42553386d6f
eugenemonnier <eugene.monnier@gmail.com>
2021-02-11 11:42:38 -0800

    added additional edge cases

--- product/product.py
+++ product/product.py
@@ -1,4 +1,8 @@
 def product(arr):
+  if len(arr) < 3: return ValueError('Input does not contain enough elements.')
+  if len(arr) == 3: 
+    print('Well that was easy.')
+    return arr[0] * arr[1] * arr[2]
   arr.sort()
   low = arr[0] * arr[1] * arr[len(arr) - 1]
   high = arr[len(arr) - 1] * arr[len(arr) - 2] * arr[len(arr) - 3]
@@ -8,3 +12,5 @@ def product(arr):
 print(product([-10, 10, 4, -3, 1, 0]))
 print(product([18, 3, 42, 17, 9, 27]))
 print(product([4, 0, -5, 3, -1, -6, 2]))
+print(product([1000, 2000]))
+print(product([100, 200, -1]))
