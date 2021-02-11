e0d2ee246727a5739c2ac1d782645974e2d7cfab
eugenemonnier <eugene.monnier@gmail.com>
2021-02-10 21:16:36 -0800

    completed challenge

--- data_structures/graph/graph.py
+++ data_structures/graph/graph.py
@@ -39,14 +39,20 @@ class Graph:
       queue.pop(0)
       
     return output
+
+  def get_edge(self, nodeA, nodeB):
+    adjacencies = self.adjacencies.get(nodeA)
+    for neighbors in adjacencies:
+      if nodeB in neighbors: return neighbors[nodeB]
+    return LookupError('Edge does not exist.')
   
 
 test_graph = Graph()
 test_graph.add_node('A')
 test_graph.add_node('B')
 print(test_graph.add_node('B'))
-test_graph.add_edge('A', 'B', 10)
-print(test_graph.add_edge('A', 'B', 10))
+test_graph.add_edge('A', 'B', 100)
+print(test_graph.add_edge('A', 'B', 100))
 print(test_graph.adjacencies)
 print(test_graph.get_nodes())
 print(test_graph.get_neighbors('A'))
@@ -57,12 +63,14 @@ test_graph.add_node('E')
 test_graph.add_node('F')
 test_graph.add_node('G')
 test_graph.add_node('H')
-test_graph.add_edge('C', 'B')
-test_graph.add_edge('C', 'G')
-test_graph.add_edge('A', 'D')
-test_graph.add_edge('B', 'D')
-test_graph.add_edge('E', 'D')
-test_graph.add_edge('F', 'D')
-test_graph.add_edge('H', 'D')
-test_graph.add_edge('H', 'F')
+test_graph.add_edge('C', 'B', 200)
+test_graph.add_edge('C', 'G', 150)
+test_graph.add_edge('A', 'D', 300)
+test_graph.add_edge('B', 'D', 450)
+test_graph.add_edge('E', 'D', 90)
+test_graph.add_edge('F', 'D', 400)
+test_graph.add_edge('H', 'D', 240)
+test_graph.add_edge('H', 'F', 420)
 print(test_graph.breadth_first('A'))
+print(test_graph.get_edge('D', 'B'))
+print(test_graph.get_edge('A', 'F'))
