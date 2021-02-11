8300a3bb9331eac8a95fa8799e2ba113e1bc4b01
eugenemonnier <eugene.monnier@gmail.com>
2021-02-11 10:50:14 -0800

    added additional solution

--- sanitize_space/sanitize_space.py
+++ sanitize_space/sanitize_space.py
@@ -9,5 +9,12 @@ def sanitize_no_builtin(url):
   sanitized += url_list[len(url_list) - 1]
   return sanitized
 
+def sanitize_list_comp(url):
+  url_list = [i for j in url.split() for i in (j, ' ')][:-1]
+  for elem in range(len(url_list)):
+    if url_list[elem] == ' ': url_list[elem] = '%20'
+  return ''.join(url_list)
+
 print(sanitize("http://code.org/hour of code.html"))
 print(sanitize_no_builtin("http://code.org/hour of code.html"))
+print(sanitize_list_comp("http://code.org/hour of code.html"))
